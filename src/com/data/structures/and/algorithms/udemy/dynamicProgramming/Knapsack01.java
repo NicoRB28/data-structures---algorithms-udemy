85127bc0f458bc48daab63a0193714bb9c155a13
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2023-02-07 00:15:05 -0300

    bottom up knapsack implementation

--- src/com/data/structures/and/algorithms/udemy/dynamicProgramming/Knapsack01.java
+++ src/com/data/structures/and/algorithms/udemy/dynamicProgramming/Knapsack01.java
@@ -8,12 +8,37 @@ public class Knapsack01 {
         int capacity = 10;
         int n = weight.length;
 
+        int maxProfitBottomUp = getMaxProfitBottomUp(weight, price, n, capacity);
+
         int[][] values = initValues(capacity, n);
 
         int maxProfit = getMaxProfit(weight, price, n, capacity, values);
 
         System.out.println("The max profit is: " + maxProfit);
+        System.out.println("The max profit is: " + maxProfitBottomUp + " (bottom up)");
 
+
+    }
+
+    // BOTTOM-UP approach
+    private static int getMaxProfitBottomUp(int[] weight, int[] price, int n, int capacity) {
+        int[][] values = new int[n+1][capacity+1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= capacity ; j++) {
+                if (i == 0 || j == 0) {
+                    values[i][j] = 0;
+                } else {
+                    int include = 0, exclude = 0;
+                    // J represents the capacity for that cell
+                    if (weight[i-1] <= j)
+                        include = price[i-1] + values[i-1][j - weight[i-1]];
+                    exclude = values[i-1][j];
+                    values[i][j] = Math.max(include, exclude);
+                }
+            }
+        }
+
+        return values[n][capacity];
     }
 
     //TOP-DOWN approach
